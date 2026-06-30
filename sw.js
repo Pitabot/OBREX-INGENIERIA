2bdcaa7a249f4d9ef0b1917f13e793b117301730
Pitabot <erickpitalu@gmail.com>
2026-06-29 23:54:29 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE = 'obrex-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'obrex-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-mark.png',
+  './logo-wordmark.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -7,7 +14,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
